08c2ddf970c6bbb2ab3739d3b5b48ddc2d78f7a9
athaa14 <147735194+athaa14@users.noreply.github.com>
2024-12-17 11:53:53 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-        return redirect()->route('loginForm');
+        return redirect()->route('loginPage');
 
     }
 }
