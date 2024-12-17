874320ceba5992926f6af8ff382c07dd319362cf
Rasyad <azrasyad65@gmail.com>
2024-12-17 13:32:51 +0700

    footer

---
+++ resources/views/components/footer-user.blade.php
@@ -0,0 +1,20 @@
+<footer class="custom-footer">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 mb-3 mb-md-0">
+          <div class="d-flex align-items-center">
+            {{-- <img src="{{ asset('assets/img/logo/logoapjifix.png') }}" alt="Company Logo" class="footer-logo me-3"> --}}
+            <p class="mb-0 footer-text">&copy; 2024 APJI. All rights reserved.</p>
+          </div>
+        </div>
+        <div class="col-md-6 text-md-end">
+          <div class="social-icons">
+            <a href="#" class="social-icon"><i class="bx bxl-facebook"></i></a>
+            <a href="#" class="social-icon"><i class="bx bxl-twitter"></i></a>
+            <a href="#" class="social-icon"><i class="bx bxl-instagram"></i></a>
+            <a href="#" class="social-icon"><i class="bx bxl-linkedin"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
